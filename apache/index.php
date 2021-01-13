35763053912a620e1796897e8e13a3c606999aea
root <root@localhost.localdomain>
2021-01-13 17:42:27 +0300

    Apache conf

---
+++ apache/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<title>Apache page</title>
+<body>
+<center>
+	<h1 style = "color:red;"> You select php page. This's Apache + PHP!</h1>
+	<img src = "apache.png" style = "float:none; width: 60%;" /> <br />
+</center>
+<center>
+<?php
+
+phpinfo();
+
+?>
+</center>
+</body>
+</hmtl>
